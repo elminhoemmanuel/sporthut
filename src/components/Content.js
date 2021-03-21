6f290664e54eb96af58f23410f7ba707f646a9da
elminhoemmanuel <amodue15@gmail.com>
2021-03-21 14:31:58 -0600

    corrected text allignment

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,7 +9,7 @@ const Content = () => {
                 <img src={imageOne} alt='ball and boot' className='h-full rounded mb-20 shadow w-4/5 md:w-1/3 '/>
                 <div className="center-content">
                         <h2 className="text-2xl mb-2">Mercurial Boot and Ball</h2>
-                        <p className="mb-2">Comfortable and well designed mercurial boots</p>
+                        <p className="mb-2 text-center">Comfortable and well designed mercurial boots</p>
                         <span>NGN 20000</span>
                 </div>
             </div>
@@ -18,7 +18,7 @@ const Content = () => {
                 <img src={imageTwo} alt='ball and boot' className='h-full rounded mb-20 shadow w-4/5 md:w-1/3'/>
                 <div className="center-content">
                         <h2 className="text-2xl mb-2">Mercurial Boot</h2>
-                        <p className="mb-2">Comfortable and well designed mercurial boots</p>
+                        <p className="mb-2 text-center">Comfortable and well designed mercurial boots</p>
                         <span>NGN 15000</span>
                 </div>
             </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="flex items-center justify-center h-16 bg-black text-white">
-            <p>COPYRIGHT © 2021 SPORTHUT ALL RIGHTS RESERVED</p>
+            <p className='text-sm'>COPYRIGHT © 2021 SPORTHUT ALL RIGHTS RESERVED</p>
         </div>
     )
 }
